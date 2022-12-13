724faf2a0d9b383ffce1363df278b8730e3d09e2
robert-jq <582153160@qq.com>
2022-12-13 12:28:39 +0800

    两数之和 解法二:使用map求解

--- src/main/java/com/robert/leetcode/twosum/TwoSumTest.java
+++ src/main/java/com/robert/leetcode/twosum/TwoSumTest.java
@@ -3,6 +3,9 @@ package com.robert.leetcode.twosum;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Robert-JQ
  * @desc 两数之和 https://leetcode-cn.com/problems/two-sum/
@@ -13,14 +16,17 @@ public class TwoSumTest {
 
     @Test
     public void twoSumTest() {
-        int[] result = twoSum1(new int[]{2, 7, 11, 15}, 22);
-        log.info("result1:{}, result2:{}", result[0], result[1]);
+        int[] result1 = twoSum1(new int[]{2, 7, 11, 15}, 22);
+        log.info("result1:{}, result2:{}", result1[0], result1[1]);
+
+        int[] result2 = twoSum2(new int[]{2, 7, 11, 15}, 22);
+        log.info("result1:{}, result2:{}", result2[0], result2[1]);
     }
 
     /**
      * @desc 两数之和解法一：用最暴力的方法穷举尝试
      * @author Robert-JQ
-     * @date 2019/4/15 0015 11:25
+     * @date 2019/4/15 11:25
      * @param nums 给定的数组
      * @param target 给定的目标值
      * @return int[]
@@ -34,6 +40,28 @@ public class TwoSumTest {
                     result[1] = m;
                 }
             }
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @author Robert-JQ
+     * @date 2022/12/12 23:21
+     * @param nums 给定的数组
+     * @param target 给定的目标值
+     * @return int[]
+     */
+    private int[] twoSum2(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>(8);
+        for (int i = 0 ; i < nums.length ; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                result[0] = i;
+                result[1] = map.get(temp);
+            }
+            map.put(nums[i], i);
         }
         return result;
     }
